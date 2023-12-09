1faf214bb928e00060e030a1ebad00184375d770
Anh Tran <trantheanh132@gmail.com>
2023-12-09 20:00:11 +0700

    [Initial]: Add Dto Order Detail Add Item and update Order

---
+++ Advanced_CSharp.DTO/Requests/Order/OrderUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿using Advanced_CSharp.Database.Enums;
+
+namespace Advanced_CSharp.DTO.Requests.Order
+{
+    public class OrderUpdateRequest
+    {
+        public Guid OrderId { get; set; }
+        public EOrderStatus Status { get; set; }
+
+    }
+}
---
+++ Advanced_CSharp.DTO/Responses/Order/OrderUpdateResponse.cs
@@ -0,0 +1,12 @@
+﻿using Advanced_CSharp.Database.Commons;
+using Advanced_CSharp.Database.Enums;
+
+namespace Advanced_CSharp.DTO.Responses.Order
+{
+    public class OrderUpdateResponse
+    {
+        public BaseResponse BaseResponse { get; set; } = new BaseResponse();
+        public EOrderStatus oldOrderStatus { get; set; } = new EOrderStatus();
+        public EOrderStatus UpdatedOrderStatus { get; set; } = new EOrderStatus();
+    }
+}
