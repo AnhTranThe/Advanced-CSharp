9cc360d45fd8d09b23e2f0652229ad7ecbcb947e
Anh Tran <trantheanh132@gmail.com>
2023-12-12 19:30:47 +0700

    [Fix]: modify base entity and const system

--- Advanced_CSharp.Database/Constants/ConstSystem.cs
+++ Advanced_CSharp.Database/Constants/ConstSystem.cs
@@ -9,6 +9,8 @@
         public const string AdminUserId = "731ACDC1-6507-4C97-86C8-9C9F135D85FE";
         public const string CustomerRoleId = "E362EB26-D015-4A22-9B24-D32B6BE4DCFA";
         public const string TesterUserId = "E26F15D9-AD4A-41FD-BD57-54A24DB7DB0B";
+        public static Guid loggedInUserId { get; set; } = Guid.Empty; // Keep track of the logged-in user
+        public static string loggedUserName { get; set; } = string.Empty;
 
 
 
